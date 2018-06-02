7925bb55e40e7ecd2746fe6af6e6364369e20e56
vlandguth <vvanderweyst@eagles.ewu.edu>
2018-06-01 20:51:27 -0700

    updated fixed header

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,11 @@
 // Your Scripts
 console.log('Hey Browser!');
 
-
-      //window scrolling
-    function resizeHeader() {
-      var _posScroll = window.scrollY;
-      var _maxScroll = 50;
-      
-      //Decrease header size
-      if (_posScroll > _maxScroll)
-      {
-        if($('.small-header').size()<=0)
-        {
-          //small on scroll
-          $('.fixed-header').addClass('small-header');
-        }
-      }
-      //back things to normal
-      else if (_posScroll < _maxScroll)
-      {
-        $('.fixed-header').removeClass('small-header');
-      }
-    }
+$(window).scroll(function(){
+  if ($(window).scrollTop() >= 120) {
+    $('.sticky-header').addClass('fixed');
+   }
+   else {
+    $('.sticky-header').removeClass('fixed');
+   }
+});
