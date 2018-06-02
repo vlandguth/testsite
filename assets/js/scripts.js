dc0759d4383c94b57ef2f3f25ce3b0f141f3c548
vlandguth <vvanderweyst@eagles.ewu.edu>
2018-06-01 19:33:00 -0700

    scripts and styles

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,31 @@
 // Your Scripts
 console.log('Hey Browser!');
+
+$(document).ready(
+    function ()
+    {
+      //window scrolling
+      $(document).scroll(function() {
+        resizeHeader();
+      });
+    });
+    
+    function resizeHeader() {
+      var _posScroll = window.scrollY;
+      var _maxScroll = 50;
+      
+      //Decrease header size
+      if (_posScroll > _maxScroll)
+      {
+        if($('.small-header').size()<=0)
+        {
+          //small on scroll
+          $('.fixed-header').addClass('small-header');
+        }
+      }
+      //back things to normal
+      else if (_posScroll < _maxScroll)
+      {
+        $('.fixed-header').removeClass('small-header');
+      }
+    }
