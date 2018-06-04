9d07c0c799885d437ca9d6de3e4bdd13116a6d78
vlandguth <vvanderweyst@eagles.ewu.edu>
2018-06-04 14:34:07 -0700

    added typeitout js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,3 +9,12 @@ $(window).scroll(function(){
     $('.sticky-header').removeClass('fixed');
    }
 });
+
+var words = ['UX Designer', 'Web Designer', 'Graduate! (soon)', 'Front-End Developer', 'UI Designer', 'Bulma Hater' ];
+setInterval(function(){
+var randomIndex = Math.floor(Math.random() * words.length); 
+var randomElement = words[randomIndex];
+    $('.type-it').typeIt({
+        content : randomElement
+    });
+}, 4000);
